87f3e91f6b1c17d645acdc708774c42dedb5bda1
Tiago Marinho <tiago.marinho.797@gmail.com>
2019-07-14 21:01:14 -0300

    Mudando layout

--- src/Components/Equipe/Equipe.js
+++ src/Components/Equipe/Equipe.js
@@ -1,19 +1,53 @@
 import React, {Component} from "react";
+import persona from '../../imgs/persona.png';
+import './equipe.css';
 
 class Equipe extends Component {
     render() {
         return (
-            <div>
-                <h2>STUFF</h2>
-                <p>Mauris sem velit, vehicula eget sodales vitae,
-                rhoncus eget sapien:</p>
-                <ol>
-                <li>Nulla pulvinar diam</li>
-                <li>Facilisis bibendum</li>
-                <li>Vestibulum vulputate</li>
-                <li>Eget erat</li>
-                <li>Id porttitor</li>
-                </ol>
+            <div className ="galeria">
+                <div id = "TechJobs">
+                    <h3>TechJobs</h3>
+                    <ul className = "linha1">
+                        <li><img src={persona} alt = "Persona"></img>
+                        <figcaption>Vinicius Costa<br></br> Gerente de Projeto</figcaption></li>
+                        <li><img src={persona} alt = "Persona"></img>
+                        <figcaption>Tiago Marinho<br></br> Gerente de Comunicação</figcaption></li>
+                        <li><img src={persona} alt = "Persona"></img>
+                        <figcaption>Igor Theotônio<br></br> Gerente de Escopo</figcaption></li>
+                        <li><img src={persona} alt = "Persona"></img>
+                        <figcaption>Andressa Oliveira<br></br> Gerente de RH</figcaption></li>
+                        <li><img src={persona} alt = "Persona"></img>
+                        <figcaption>Glauber Leite<br></br> Gerente de Tempo </figcaption></li>
+                        <li><img src={persona} alt = "Persona"></img>
+                        <figcaption>Romário Pantaleão<br></br> Gerente de Stakeholders </figcaption></li>
+                        <li><img src={persona} alt = "Persona"></img>
+                        <figcaption>José Carlos<br></br> Gerente de Aquisição </figcaption></li>
+                        <li><img src={persona} alt = "Persona"></img>
+                        <figcaption>Gabriel Freire <br></br> Gerente de Custos </figcaption></li>
+                        <li><img src={persona} alt = "Persona"></img>
+                        <figcaption>Ziran de Moura<br></br> Gerente de Qualidade</figcaption></li>
+                        <li><img src={persona} alt = "Persona"></img>
+                        <figcaption>Dayvson Sales<br></br> Gerente de Riscos </figcaption></li>
+                        <li><img src={persona} alt = "Persona"></img>
+                        <figcaption>Caue dos Santos<br></br> Operacional</figcaption></li>
+                        <li><img src={persona} alt = "Persona"></img>
+                        <figcaption>José Barros<br></br> Operacional</figcaption></li>
+                        <li><img src={persona} alt = "Persona"></img>
+                        <figcaption>Luan Viana<br></br>Operacional </figcaption></li>
+                        
+                    </ul>
+                </div>
+
+                <div id = "TechTrends">
+                    <h3>TechTrends</h3>
+                    <ul className = "linha1">
+                        <li><img src={persona} alt = "Persona"></img></li>
+                        <li><img src={persona} alt = "Persona"></img></li>
+                        <li><img src={persona} alt = "Persona"></img></li>
+                    </ul>
+                </div>
+                
             </div>
         );
     }
--- src/Main.js
+++ src/Main.js
@@ -4,6 +4,8 @@ import{
     Route, NavLink, HashRouter
 } from "react-router-dom";
 
+import backgroundHeader from './imgs/background-header2.jpg';
+
 import Principal from "./Components/Principal/Principal";
 import Contato from "./Components/Contato/Contato";
 import Equipe from "./Components/Equipe/Equipe";
@@ -14,28 +16,36 @@ import Techjobs from "./Components/Techjobs/Techjobs";
 class Main extends Component {
     render() {
         return (
-            <HashRouter>
-                <div>
-                <h1>TechDay</h1>
-                <ul className="header">
-                <li><NavLink exact to="/">Principal</NavLink></li>
-                <li><NavLink to="/techjobs">TechJobs</NavLink></li>
-                <li><NavLink to="/techtrends">TechTrends</NavLink></li>
-                <li><NavLink to="/inscreva">Inscreva-se</NavLink></li>
-                <li><NavLink to="/equipe">Equipe</NavLink></li>
-                <li><NavLink to="/contato">Contate-nos</NavLink></li>
-                </ul>
-
-                <div className="content">
-                <Route exact path="/" component={Principal}/>
-                <Route path="/techjobs" component={Techjobs}/>
-                <Route path="/techtrends" component={Techtrends}/>
-                <Route path="/inscreva" component={Inscreva}/>
-                <Route path="/equipe" component={Equipe}/>
-                <Route path="/contato" component={Contato}/>
+            <div>
+                <div className = "btn-wrap">
+                    <img id = "backgroundHeader" src={backgroundHeader} alt = "Persona"></img>
                 </div>
+
+                <HashRouter>
+                    <div className="Main-Content">
+                        
+
+                        <header className="header">
+                            <ul className="navBar">
+                                <li><NavLink exact to="/">Principal</NavLink></li>
+                                <li><NavLink to="/techjobs">TechJobs</NavLink></li>
+                                <li><NavLink to="/techtrends">TechTrends</NavLink></li>
+                                <li><NavLink to="/inscreva">Inscreva-se</NavLink></li>
+                                <li><NavLink to="/equipe">Equipe</NavLink></li>
+                                <li><NavLink to="/contato">Contate-nos</NavLink></li>
+                            </ul>
+                        </header>
+                        <div className="body">
+                            <Route exact path="/" component={Principal}/>
+                            <Route path="/techjobs" component={Techjobs}/>
+                            <Route path="/techtrends" component={Techtrends}/>
+                            <Route path="/inscreva" component={Inscreva}/>
+                            <Route path="/equipe" component={Equipe}/>
+                            <Route path="/contato" component={Contato}/>
+                        </div>
+                    </div>
+                </HashRouter>
             </div>
-            </HashRouter>
         );
     }
 }
